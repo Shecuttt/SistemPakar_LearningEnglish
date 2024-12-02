915fb38ab5c51f06368082737d3b25a5ff0c9c21
iyanalmasalbantani <99644683+Iyanalmas@users.noreply.github.com>
2024-12-02 23:15:14 +0700

    ubah riwayat dikit

--- src/app/riwayat/page.js
+++ src/app/riwayat/page.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import Riwayat from "../components/Riwayat";
 
 export default function page() {
   return (
     <div className="min-h-screen bg-sesi">
       <Navbar />
+      <div className="container mx-auto p-4 pt-6 mt-20">
+        <Riwayat />
+      </div>
     </div>
   );
 }
